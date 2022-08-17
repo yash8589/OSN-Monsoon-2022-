29938e835c61789eea2e0b2c9251a7e600f23b8b
Yash Agrawal <yash.a@research.iiit.ac.in>
2022-08-18 00:17:22 +0530

    working 2nd Q

--- A1/2.c
+++ A1/2.c
@@ -29,7 +29,7 @@ Reverse the file except the part that that is specified at the start of the exec
 int main(int argc, char *argv[])
 {
     // check if the file address has been provided
-    if (argc != 2)
+    if (argc != 4)
     {
         write(2, "ERROR!!!\n", 10);
         exit(1);
@@ -69,6 +69,9 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
+    long long begin = atoi(argv[2]);
+    long long end = atoi(argv[3]);
+
     // cursor at the beginning of the output file
     lseek(dest_file, 0, SEEK_SET);
     // cursor at the end of the input file
@@ -101,6 +104,7 @@ int main(int argc, char *argv[])
 
     long long total_progress = 0;
     char buffer[100];
+    char message[100];
 
     // printf("\nFileLength: %ld \nNo of bytes per read: %d \nStart: %d \nChunk Size: %d\n\n", fileLength, chunksize, start, len_to_move);
     // main loop
@@ -112,7 +116,12 @@ int main(int argc, char *argv[])
         // TODO: make a functioning progressbar
         ssize_t read_size = read(source_file, c, len_to_move);
         total_progress += read_size;
-        printf(buffer, "\r progress = %.2f%%", (float)(total_progress * 100) / fileLength);
+        // sprintf(message, "\r Reversing the entire file\n");
+        // write(STDOUT_FILENO, message, strlen(message));
+        // fflush(stdout);
+        sprintf(buffer, "\r progress = %.2f%%", (100.0f * total_progress) / fileLength);
+        write(STDOUT_FILENO, buffer, strlen(buffer));
+        fflush(stdout);
 
         // printf("%s\n", c);
         // reversing the chunk of bytes
@@ -127,12 +136,74 @@ int main(int argc, char *argv[])
         // printf("%s\n", r);
         if (lseek(source_file, 0, SEEK_CUR) - chunksize - len_to_move < 0)
         {
-            close(source_file);
-            close(dest_file);
             break;
         }
         lseek(source_file, -chunksize - len_to_move, SEEK_CUR);
         len_to_move = chunksize;
     }
+
+    // lseek(dest_file, 0, SEEK_SET);
+    // // cursor at the end of the input file
+    // lseek(source_file, -1, SEEK_END);
+
+    // creation of chunks to optimize the working of the code
+    byte_size = end - begin;
+    chunksize = byte_size;
+
+    // printf("%d\n", chunksize);
+
+    // c is the chunk of bytes to be read, and r is the reverse of c
+    char *c1 = (char *)malloc(sizeof(char) * chunksize);
+    char *r1 = (char *)malloc(sizeof(char) * chunksize);
+
+    // index of the last chunk of the input file
+    start = fileLength - begin;
+    // length of the last chunk of the input file
+    len_to_move = byte_size;
+    // cursor at the end of the input file
+    lseek(dest_file, fileLength - end, SEEK_SET);
+
+    total_progress = 0;
+    char buffer1[100];
+    char message1[100];
+
+    // printf("\nFileLength: %ld \nNo of bytes per read: %d \nStart: %d \nChunk Size: %d\n\n", fileLength, chunksize, start, len_to_move);
+    // main loop
+    while (1)
+    {
+        // printf("\nLocation: %lld\tLength read: %d\t", lseek(source_file, 0, SEEK_CUR), len_to_move);
+        // reading the chunk of bytes
+
+        // TODO: make a functioning progressbar
+        ssize_t read_size = read(dest_file, c1, len_to_move);
+        total_progress += read_size;
+        // sprintf(message1, "\r Reversing the middle section\n");
+        // write(STDOUT_FILENO, message1, strlen(message1));
+        // fflush(stdout);
+        sprintf(buffer1, "\r progress = %.2f%%", (100.0f * total_progress) / (end - begin));
+        write(STDOUT_FILENO, buffer1, strlen(buffer1));
+        fflush(stdout);
+
+        // printf("%s\n", c);
+        // reversing the chunk of bytes
+        for (long long i = 0; i < len_to_move; i++)
+        {
+            r1[i] = c1[len_to_move - i - 1];
+        }
+        r1[len_to_move] = ' ';
+        // printf("%s\n", r);
+        // writing the reversed chunk of bytes to the output file
+        lseek(dest_file, fileLength - end, SEEK_SET);
+        write(dest_file, r1, len_to_move);
+        // printf("%s\n", r1);
+        if (lseek(source_file, 0, SEEK_CUR) - chunksize - len_to_move < 0)
+        {
+            break;
+        }
+        lseek(source_file, -chunksize - len_to_move, SEEK_CUR);
+        len_to_move = chunksize;
+    }
+    close(source_file);
+    close(dest_file);
     return 0;
 }
